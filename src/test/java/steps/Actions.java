b5ac282b33f06f07da3c2ed29119588f37ff7bce
fzabala <S3l3nium>
2021-03-10 09:10:09 -0500

    First feature

--- src/test/java/steps/Actions.java
+++ src/test/java/steps/Actions.java
@@ -5,5 +5,6 @@ import io.cucumber.java.en.Given;
 public class Actions {
     @Given("the user wants to buy a guitar")
     public void theUserWantsToBuyAGuitar() {
+        System.out.println("Test");
     }
 }
